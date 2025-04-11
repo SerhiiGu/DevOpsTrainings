8a80217084e0c00bf7ad395d55851edde842c139
Serhii <102320885+SerhiiGu@users.noreply.github.com>
2025-04-12 01:41:28 +0300

    Create App.js

---
+++ task-1/todo-app/web/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>ToDo App</h1>
+      <input
+        type="text"
+        placeholder="Enter task"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
