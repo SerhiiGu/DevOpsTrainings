767ca63178c70f6eaf9c360d610d6023fe47a031
Serhii <102320885+SerhiiGu@users.noreply.github.com>
2025-04-12 22:28:36 +0300

    Update variables.tf

--- task-0/terraform-docker-swarm/variables.tf
+++ task-0/terraform-docker-swarm/variables.tf
@@ -1,15 +1,18 @@
-variable "manager" {
-  type = object({
-    ip       = string
-    user     = string
-    ssh_key  = string
-  })
+variable "swarm_manager_ip" {
+  description = "Target IP for Docker Swarm Manager Server"
+  type        = string
 }
 
-variable "worker" {
-  type = object({
-    ip       = string
-    user     = string
-    ssh_key  = string
-  })
+variable "swarm_worker_ip" {
+  description = "Target IP for Docker Swarm Worker Server"
+  type        = string
+}
+
+variable "ssh_private_key_path" {
+  default     = "~/.ssh/id_rsa"
+  description = "Path to the SSH private key"
+}
+
+variable "ssh_user" {
+  default     = "root"
 }
