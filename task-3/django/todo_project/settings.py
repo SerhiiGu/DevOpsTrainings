f49e8053dc529805ade923191ebf7d78c4423204
Serhii <102320885+SerhiiGu@users.noreply.github.com>
2025-04-15 16:31:25 +0300

    Update settings.py

--- task-3/django/todo_project/settings.py
+++ task-3/django/todo_project/settings.py
@@ -59,7 +59,7 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD', 'todo_pass'),
         'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 5432),
-        'CONN_MAX_AGE': '60',
+        'CONN_MAX_AGE': 60,
     }
 }
 
