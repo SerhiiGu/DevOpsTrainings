951c850bd8707d38932bf1e56696e5963ea3586b
Serhii <102320885+SerhiiGu@users.noreply.github.com>
2025-04-15 11:48:35 +0300

    Update entrypoint.sh

--- task-3/django/entrypoint.sh
+++ task-3/django/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Wait for DB
 echo "Waiting for PostgreSQL..."
-while ! python -c "import psycopg2; psycopg2.connect(host='$DB_HOST', port=$DB_PORT, dbname=$DB_NAME, user=$DB_USER, password=$DB_PASSWORD)" > /dev/null 2>&1; do
+while ! python -c "import psycopg2; psycopg2.connect(host='$DB_HOST', port=$DB_PORT, dbname='$DB_NAME', user='$DB_USER', password='$DB_PASSWORD')" > /dev/null 2>&1; do
   sleep 0.5
 done
 
