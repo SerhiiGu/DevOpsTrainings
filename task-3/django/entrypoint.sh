77b3a664e6e12162d37996a1eb1e8fc60cab6ff9
Serhii <102320885+SerhiiGu@users.noreply.github.com>
2025-04-15 11:30:41 +0300

    Update entrypoint.sh

--- task-3/django/entrypoint.sh
+++ task-3/django/entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Wait for DB
+echo "Waiting for PostgreSQL..."
+while ! nc -z $SQL_HOST $SQL_PORT; do
+  sleep 0.5
+done
+
+
+echo "PostgreSQL started"
+
+# Run migrations and start app
 python manage.py migrate
 python manage.py collectstatic --noinput
-exec "$@"
+exec gunicorn todo_project.wsgi:application --bind 0.0.0.0:8000
+
