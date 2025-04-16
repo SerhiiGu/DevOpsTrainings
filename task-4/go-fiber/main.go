d585334abfd434d465cc056b9f81b2f42f0f693d
Serhii <102320885+SerhiiGu@users.noreply.github.com>
2025-04-16 10:46:36 +0300

    Create main.go

---
+++ task-4/go-fiber/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/gofiber/adaptor/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello from Go App 🐹")
+	})
+
+	app.Get("/metrics", adaptor.HTTPHandler(promhttp.Handler()))
+
+	app.Listen(":4000")
+}
