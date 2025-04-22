4f394e9ae8c2b2907241de1f57310716d65b29c8
SerhiiGu <SerhiiGu@admin>
2025-04-22 20:07:04 +0300

    update ansible

--- task-5/ansible_zero-downtime_rolling_update/update_db.sh
+++ task-5/ansible_zero-downtime_rolling_update/update_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-playbook -i inventory playbooks/update_service.yml -e "service_name=db"
+ansible-playbook -i inventory playbooks/update_service.yml -e "service_name=db" -e "image=mysql" -e "image_tag=8"
 
--- task-5/ansible_zero-downtime_rolling_update/update_nginx.sh
+++ task-5/ansible_zero-downtime_rolling_update/update_nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-playbook -i inventory playbooks/update_service.yml -e "service_name=nginx"
+ansible-playbook -i inventory playbooks/update_service.yml -e "service_name=nginx" -e "image=nginx" -e "image_tag=alpine"
 
