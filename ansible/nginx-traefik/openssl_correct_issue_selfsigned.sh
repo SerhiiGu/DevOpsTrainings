f83f878889507f31437cce293d7cc54a88bdd0f5
SerhiiGu <SerhiiGu@admin>
2025-04-23 23:44:04 +0300

    selfsigned SSL for multiple hosts works

--- ansible/nginx-traefik/openssl_correct_issue_selfsigned.sh
+++ ansible/nginx-traefik/openssl_correct_issue_selfsigned.sh
@@ -13,6 +13,12 @@ openssl req -x509 -newkey rsa:4096 -nodes \
   -subj "/CN=traefik.local" \
   -addext "subjectAltName=DNS:traefik.local"
 
+openssl req -x509 -newkey rsa:4096 -nodes \
+  -keyout qweasd123.key -days 365 \
+  -out qweasd123.crt \
+  -subj "/CN=qweasd123.local" \
+  -addext "subjectAltName=DNS:qweasd123.local"
+
 
 #openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem \
 #  -out cert.pem -days 365 \
