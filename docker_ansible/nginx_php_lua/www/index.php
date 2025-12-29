79e7cf1475434911e4d566418fdd9f82427f9b00
SerhiiGu <SerhiiGu@admin>
2025-12-29 13:34:31 +0200

    polishing

--- docker_ansible/nginx_php_lua/www/index.php
+++ docker_ansible/nginx_php_lua/www/index.php
@@ -1,11 +1,10 @@
 <?php
 header('Content-Type: application/json');
 
-// Отримуємо всі заголовки
 $headers = getallheaders();
 
 echo json_encode([
-    "message" => "PHP отримав запит",
+    "message" => "PHP got request",
     "received_headers" => $headers,
     "raw_post" => file_get_contents('php://input')
 ], JSON_PRETTY_PRINT);
